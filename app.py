af960e6bbc1bbdafec9d97ea372660193b3c1b23
sumitgangwani <sumitgangwani99@gmail.com>
2024-01-21 23:30:00 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# Importing required functions
+
 from flask import Flask, render_template, jsonify, request, session, flash, redirect
 import sqlite3
 import os
-import requests
 
-# Flask constructor
+
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -14,7 +14,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Function to get historical data based on query parameters
+
 def get_historical_data(symbol, from_date, to_date):
     connection = sqlite3.connect("database/truebacon.db")
     cursor = connection.cursor()
@@ -33,12 +33,12 @@ def get_historical_data(symbol, from_date, to_date):
 
 
 
-# Root endpoint
+
 @app.route('/')
 def render_start_page():
-    return render_template('start.html')
+    return render_template('index.html')
 
-# User Authentication Logic
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
@@ -60,7 +60,7 @@ def signin():
     return render_template('signin.html')
 
 
-# User Registration Logic
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -79,41 +79,41 @@ def signup():
     return render_template('signup.html')
 
 
-# Modify the 'dashboard' route in app.py
+
 @app.route('/dashboard')
 def dashboard():
-    # Check if the user is authenticated
-    if 'username' not in session:
-        return redirect('/signin')  # Redirect to signin page if not authenticated
 
-    # Fetch data from the holdings table
+    if 'username' not in session:
+        return redirect('/signin')
+
+
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM holdings WHERE user_id = ?", (session['username'],))
     holdings_data = cursor.fetchall()
 
-    # Calculate total PNL
+
     total_pnl = sum(holding['pnl'] for holding in holdings_data)
 
     conn.close()
 
-    # Render the dashboard template with the fetched data and total PNL
+
     return render_template('dashboard.html', holdings_data=holdings_data, total_pnl=total_pnl)
 
 @app.route('/profile')
 def profile():
-    # Check if the user is authenticated
-    if 'username' not in session:
-        return redirect('/signin')  # Redirect to signin page if not authenticated
 
-    # Fetch data from the profile table
+    if 'username' not in session:
+        return redirect('/signin')
+
+
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM profile WHERE user_id = ?", (session['username'],))
     user_profile = cursor.fetchone()
     conn.close()
 
-    # Render the profile template with the fetched data
+
     return render_template('profile.html', user_profile=user_profile)
 
 
@@ -133,17 +133,17 @@ def place_order():
         price = float(request.form['price'])
         quantity = int(request.form['quantity'])
 
-        # Simulate API call to get place_order_response.json
+
         place_order_response = {
             "status": "success",
             "message": f"Order placed for {quantity} shares of {symbol} at {price} per share."
         }
 
-        # Display the response in JSON format
+
         return jsonify(place_order_response)
 
 
-# API endpoint to get historical data
+
 @app.route('/historical-data', methods=['GET'])
 def get_historical_api():
     symbol = request.args.get('symbol')
@@ -164,9 +164,8 @@ def logout():
     session.pop('username', None)
     return redirect('/')
 
-# Main Driver Function
+
 if __name__ == '__main__':
-    # Run the application on the local development server
     app.run(debug=True)
 
 
--- db_insertion.py
+++ db_insertion.py
@@ -15,7 +15,7 @@ def create_database(database_name):
         )
     ''')
 
-    # Create a table for user profiles
+    ## table creation
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS profile (
             user_id TEXT,
@@ -27,7 +27,7 @@ def create_database(database_name):
         )
     ''')
 
-    # Create a table for holdings
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS holdings (
             user_id TEXT,
@@ -45,13 +45,13 @@ def create_database(database_name):
         )
     ''')
 
-    # Insert mock entry into profile table
+
     cursor.execute('''
         INSERT INTO profile (user_id, user_type, email, user_name, broker, password)
         VALUES (?, ?, ?, ?, ?, ?)
     ''', ("AB1234", "individual", "xxxyyy@gmail.com", "AxAx Bxx", "ZERODHA", "1234"))
 
-    # Insert mock data into holdings table
+
     holdings_data = [
         ("AB1234","GOLDBEES", "BSE", "INF204KB17I5", 2, "2021-06-08 00:00:00", 40.67, 42.47, 42.28, 3.6, 0.19, 0.44938505203405327),
         ("AB1234","IDEA", "NSE", "INE669E01016", 5, "2021-06-08 00:00:00", 8.466, 10, 10.1, 7.67, -0.1, -0.9900990099009866)
@@ -85,16 +85,16 @@ def insert_data_from_csv(database_name, csv_file_path):
     connection.close()
 
 if __name__ == "__main__":
-    # Specify the SQLite database name
+
     db_name = "database/truebacon.db"
 
-    # Specify the path to your CSV file
+
     csv_path = "historical_prices.csv"
 
-    # Create the database and tables, including profile with mock entry and holdings with mock data
+
     create_database(db_name)
 
-    # Insert data from CSV into the nifty table
+
     insert_data_from_csv(db_name, csv_path)
 
     print("Data inserted successfully.")
